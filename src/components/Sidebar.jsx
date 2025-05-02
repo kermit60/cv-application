39fc8fb1f5c727a873c50d0a37c57857e0a21f16
kermit60 <112352377+kermit60@users.noreply.github.com>
2025-05-01 21:23:24 -0400

    Implement download cv button and feature

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,6 +4,7 @@ import SkillsSection from './SkillsSection'
 import EducationSection from './EducationSection'
 import ProjectsSection from './ProjectsSection'
 import ExperienceSection from './ExperienceSection'
+import DownloadIcon from '../assets/download.svg';
 
 export default function Sidebar({
   info, setInfo,
@@ -12,14 +13,30 @@ export default function Sidebar({
   experiences, setExperience,
   projects, setProjects
 }) {
+
+  const downloadCV = () => {
+    window.print();
+    setTimeout(() => {
+      document.title = info.name ? info.name + " Resume" : "";
+    }, 100);
+  };
+
+  const style = {
+    backgroundColor: "green"
+  }
+
   return (
     <div className="sidebar">
+      <div className='download-button'>
+        <button type='button' className='button' style={style} onClick={downloadCV}>
+          <img className='download' src={DownloadIcon} alt="Download Icon" /> Download CV
+        </button>
+      </div>
       <Personal info={info} setInfo={setInfo}/>
       <SkillsSection skills={skills} setSkills={setSkills}/>
       <ExperienceSection experiences={experiences} setExperience={setExperience}/>
       <ProjectsSection projects={projects} setProjects={setProjects} />
       <EducationSection education={education} setEducation={setEducation}/>
-
     </div>
   )
 }
